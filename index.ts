7e77fe70c6d5c7369721697e6dcf125c21c40cc7
Ankit Panchal <ankitpanchal534@gmail.com>
2025-05-25 17:08:51 +0530

    added exports and update package version to 1.0.8

--- index.ts
+++ index.ts
@@ -2,6 +2,33 @@
 export { useClickAnywhere } from "./src/hooks/useClickAnywhere";
 export { useKeyPress } from "./src/hooks/useKeyPress";
 export { useWindowResize } from "./src/hooks/useWindowResize";
+export { useDebounce } from "./src/hooks/useDebounce";
+export { useInterval } from "./src/hooks/useInterval";
+export { useLocalStorage } from "./src/hooks/useLocalStorage";
+export { useOnScreen } from "./src/hooks/useOnScreen";
+export { usePrevious } from "./src/hooks/usePrevious";
+export { useTimeout } from "./src/hooks/useTimeout";
+export { useToggle } from "./src/hooks/useToggle";
+export { useFocusTrap } from "./src/hooks/useFocusTrap";
+export { useMediaQuery } from "./src/hooks/useMediaQuery";
+export { useOnClickOutside } from "./src/hooks/useOnClickOutside";
+export { useScrollPosition } from "./src/hooks/useScrollPosition";
+export { useClipboard } from "./src/hooks/useClipboard";
+export { useConditionalEffect } from "./src/hooks/useConditionalEffect";
+export { useDocumentTitle } from "./src/hooks/useDocumentTitle";
+export { useElementSize } from "./src/hooks/useElementSize";
+export { useFavicon } from "./src/hooks/useFavicon";
+export { useHover } from "./src/hooks/useHover";
+export { useIsMounted } from "./src/hooks/useIsMounted";
+export { useOnlineStatus } from "./src/hooks/useOnlineStatus";
+export { usePageExit } from "./src/hooks/usePageExit";
+export { useRenderCount } from "./src/hooks/useRenderCount";
+export { useScrollDirection } from "./src/hooks/useScrollDirection";
+export { useSessionStorage } from "./src/hooks/useSessionStorage";
+export { useThrottle } from "./src/hooks/useThrottle";
+export { useTimeoutToggle } from "./src/hooks/useTimeoutToggle";
+export { useUpdateEffect } from "./src/hooks/useUpdateEffect";
+export { useWhyDidYouUpdate } from "./src/hooks/useWhyDidYouUpdate";
 
 // utils
 export { printMsg } from "./src/utils/printMsg";
